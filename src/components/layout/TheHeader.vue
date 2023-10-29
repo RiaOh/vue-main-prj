fd3dbc295da5308a0319a9542f7ee4906b986fc2
RiaOh <ken5840714@gmail.com>
2023-10-29 23:34:12 +0900

    Section26 238까지

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav>
+      <h1><router-link to="/">Find a Coach</router-link></h1>
+      <ul>
+        <li><router-link to="/coaches">All Coaches</router-link></li>
+        <li><router-link to="/requests">Requests</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  color: red;
+}
+a {
+  text-decoration: none;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: CoachesList },
+    {
+      path: '/coaches/:id',
+      component: CoachDetail,
+      children: [{ path: 'contact', component: RequestsReceived }],
+    },
+    { path: '/register', component: CoachRegistration },
+    { path: '/requests', component: ContactCoach },
+    { path: '/:notFound(.*)', component: NotFound },
+  ],
+});
+export default router;
