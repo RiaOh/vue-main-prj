4800d5bbd288079265c968df019ddf7493cac010
RiaOh <ken5840714@gmail.com>
2023-10-31 23:45:52 +0900

    2023.10.31. -242까지

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,20 +1,36 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="control">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <ul v-if="hasCoachees">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+    <base-card>
+      <div class="controls">
+        <!-- <button>Refresh</button> -->
+        <base-button mode="flat">Refresh</base-button>
+        <!-- <router-link to="/register">Register as Coach</router-link> -->
+        <base-button link="true" mode="outline">Register as Coach</base-button>
+      </div>
+      <ul v-if="hasCoachees">
+        <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
-      </li>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
+      </li> -->
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :firstName="coach.firstName"
+          :lastName="coach.lastName"
+          :rate="coach.hourlyRate"
+          :areas="coach.areas"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+  components: { CoachItem },
   computed: {
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
@@ -28,3 +44,15 @@ export default {
   },
 };
 </script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
